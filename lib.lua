4fc2a1a1a9702dc535e9ae2e19546bf987328e57
Ziggy <96440226+NotZiggy@users.noreply.github.com>
2022-02-28 14:56:53 +0100

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -80,7 +80,7 @@ function library:CreateWatermark(name, position)
     watermark.main = Instance.new("ScreenGui", coregui)
     watermark.main.Name = "Watermark"
     if syn then
-        --syn.protect_gui(watermark.main)
+        syn.protect_gui(watermark.main)
     end
 
     if getgenv().watermark then
@@ -232,7 +232,7 @@ function library:CreateWindow(name, size, hidebutton)
     window.Main.Name = name
     window.Main.DisplayOrder = 15
     if syn then
-        syn.protect_gui(window.Main)
+        --syn.protect_gui(window.Main)
     end
 
     if getgenv().uilib then
